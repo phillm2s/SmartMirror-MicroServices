dadbd5b7df52d992fdf4c7776b11da049db56b3c
phillm2s <patrick.hillmann@smail.inf.h-brs.de>
2021-06-10 12:59:22 +0200

    set iframe callback for corona ms

--- iframeParentCallback.js
+++ iframeParentCallback.js
@@ -1,11 +1,12 @@
 
 function sendSucessfullLoadedCallback(optionalParameter ={}){
-
     var dto ={
         height: document.documentElement.scrollHeight,
         width: document.documentElement.scrollWidth,
         url: window.location.href,
         optional: optionalParameter
     };
+    console.log("send callback:");
+    console.log(dto);
     parent.postMessage(dto,"*");
 }
