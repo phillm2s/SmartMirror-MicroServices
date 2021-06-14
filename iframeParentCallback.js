f8b013dfa39a47713da05182cfcd4ece66ae6af0
phillm2s <patrick.hillmann@smail.inf.h-brs.de>
2021-06-14 20:26:25 +0200

    Implementetd a new rezising concept
    -client sends on  vanilla ReziseObeser() event.
    -new size caluculation(with margin still a bit buggy)
    -cleaned mirrror componentmanager a bit. including two types of events. 1) unique event to make sure the iframe is loaded. 2)  repeadable eventListener for rezising

--- iframeParentCallback.js
+++ iframeParentCallback.js
@@ -2,9 +2,10 @@
  * Optional Parameter could be: {expirationTimeSec: 30*60}
  */
 function sendSucessfullLoadedCallback(optionalParameter ={}){
-    var dto ={
-        height: document.body.offsetHeight,
-        width: document.body.offsetWidth, 
+    var dto = {
+        height: document.body.scrollHeight + "px", 
+        // only margin right will is not included...
+        width: (document.body.scrollWidth + parseInt(window.getComputedStyle(document.body).marginRight.slice(0,-2))) + "px", 
         url: window.location.href,
         optional: optionalParameter
     };
@@ -12,6 +13,23 @@ function sendSucessfullLoadedCallback(optionalParameter ={}){
     console.log(dto);
     parent.postMessage(dto,"*");
 }
+
+function useDefaultCallbackAndReziseHandler(optionalParameter = {}){
+    var lastBodyzise = {h: -1, w: -1};
+    new ResizeObserver(()=>{
+        //filer if body size didnt changed
+        var currentBodySize = {
+            h: document.body.offsetHeight,
+            w: document.body.offsetWidth,
+        }
+        if (currentBodySize.w != lastBodyzise.w || currentBodySize.h != lastBodyzise.h) {
+            sendSucessfullLoadedCallback(optionalParameter);
+            lastBodyzise = currentBodySize;
+        }
+    }).observe(document.documentElement); // observe the html element not the body to include margin and padding from boddy
+}
+
+// Shorter: new ResizeObserver(()=>{...}).observe(HTMLElment)
 
 // function enableCallBackOnBodyRezise() {
 //     sendSucessfullLoadedCallback(); // make sure it will send at least once
--- mirror/componentManager.js
+++ mirror/componentManager.js
@@ -6,6 +6,8 @@ class View {
         this.parameters = serviceParameter = serviceParameter || {};
         this.url = url;
 
+        var connectionFailedCounter = 0;
+
         var urlWithParams = url;
         var c = "?";  //start char for get request
         for (var key in serviceParameter) {
@@ -13,44 +15,12 @@ class View {
             c = "&"; // first param with "?" prefix
         }
 
-        var loadIFrameContent = function(iframe, timeout=1000*15) { // 15sec
-            return new Promise(function(resolve,reject){
-
-                var timeoutHandler = setTimeout(()=>{
-                    window.removeEventListener("message", messageReceivedEvent, false);
-                    reject();
-                },timeout);
-                var messageReceivedEvent = function(event) {
-                    //make sure its the callback from the correct iframe
-                    if (event.data.url === urlWithParams) {
-                        clearTimeout(timeoutHandler);
-                        window.removeEventListener("message", messageReceivedEvent, false);
-                        resolve(event);
-                    }
-                }
-                window.addEventListener("message", messageReceivedEvent, false);
-
-                iframe.src = urlWithParams;
-            });
-        };
-        var hideFrame = function(iframe) {
-            iframe.style.visibility="hidden";
-        };
-        var showFrame = function(iframe){
-            iframe.style.visibility="visible";
-        };
-
-        //init iframes
+        // --- init iframes -------------------
         this.frame1 = document.createElement("iframe");
         this.frame2 = document.createElement("iframe");
-        //after an duration remove iframe borders;
-        setTimeout(()=>{
-            instance.frame1.style.border = "0";
-            instance.frame2.style.border = "0";
-        }, 1000*30);
 
-        this.activeFrame = this.frame2; //inertital state
-        this.inaktiveFrame = this.frame1;
+        this.activeFrame = this.frame1;
+        this.inaktiveFrame = this.frame2;
         [this.frame1, this.frame2].forEach((frame)=>{
             frame.className = name;
 
@@ -68,45 +38,106 @@ class View {
             hideFrame(frame);
             document.body.appendChild(frame);
         });
+        // --------------------
 
-        var instance = this;
-        var connectionFailedCounter = 0;
-        var refreshContent = function() {
+
+        //after an duration remove iframe borders;
+        setTimeout(()=>{
+            instance.frame1.style.border = "0";
+            instance.frame2.style.border = "0";
+        }, 1000*30);
+
+
+        refreshContent();
+
+        function loadIFrameContent(iframe, timeout=1000*15) { // 15sec
+            return new Promise(function(resolve,reject){
+
+                var timeoutHandler = setTimeout(()=>{
+                    window.removeEventListener("message", iframeLoadedSucessfullEvent, false);
+                    window.removeEventListener("message", repeatableIframeCallback, false);
+                    reject();
+                },timeout);
+
+                window.addEventListener("message", iframeLoadedSucessfullEvent, false);
+
+                // remove event listenet and append it again to make sute there is never more then one "iframeCallbackHandler" listener.
+                window.removeEventListener("message", repeatableIframeCallback, false);
+                window.addEventListener("message", repeatableIframeCallback, false);
+
+                iframe.src = urlWithParams;
+
+                // messageRecivedEvent cant trigger only once!
+                function iframeLoadedSucessfullEvent(event) {
+                    //make sure its the callback from the correct iframe
+                    if (event.data.url === urlWithParams) {
+                        clearTimeout(timeoutHandler);
+                        window.removeEventListener("message", iframeLoadedSucessfullEvent, false);
+
+                        console.log("\n+++++++++++++++++++++++++\nRecived loaded callback from service '" + name + "'.");
+                        console.log(event.data);
+                        console.log("+++++++++++++++++++++++++\n");
+
+                        resolve(event);
+                    }
+                }
+
+                function repeatableIframeCallback(event) {
+                    if (event.data.url === urlWithParams) {
+                        console.log("\n+++++++++++++++++++++++++\nRecived repeatable callback from '" + name + "'.");
+                        console.log(event.data);
+                        console.log("+++++++++++++++++++++++++\n");
+                        
+                        [instance.frame1, instance.frame2].forEach((frame)=>{
+                            //size priority order: ServiceParameter >> Callback >> default
+                            frame.width = event.data.positioningParameter?.width || event.data.width || instance.inaktiveFrame.width;
+                            frame.height = event.data.positioningParameter?.height || event.data.height || instance.inaktiveFrame.height;
+                        });
+                    }
+                }
+            });
+        };
+
+        function hideFrame(iframe) {
+            iframe.style.visibility="hidden";
+        };
+
+        function showFrame(iframe){
+            iframe.style.visibility="visible";
+        };
+
+        function refreshContent() {
             loadIFrameContent(instance.inaktiveFrame)
                 .then((iFrameCallback)=>{
                     connectionFailedCounter = 0; //reset failures
-                    console.log("Loading " + name + " succesfull.");
-                    console.log(iFrameCallback);
-                    let d = iFrameCallback.data;
-                    //size priority order: ServiceParameter >> Callback >> default
-                    instance.inaktiveFrame.width = positioningParameter.width || d.width || instance.inaktiveFrame.width;
-                    instance.inaktiveFrame.height = positioningParameter.height || d.height || instance.inaktiveFrame.height;
+
                     hideFrame(instance.activeFrame);
                     showFrame(instance.inaktiveFrame);
+
                     //swap frames
                     let tmp = instance.activeFrame;
                     instance.activeFrame = instance.inaktiveFrame;
                     instance.inaktiveFrame = tmp;
 
+                    let d = iFrameCallback.data;
                     if (d.optional.expirationTimeSec != null && d.optional.expirationTimeSec >=10){ //min 10 sec
-                        console.log("Refresh " + name + " in " + d.optional.expirationTimeSec + " sec.")
+                        console.log("Refresh " + name + " in " + d.optional.expirationTimeSec + " sec.");
                         setTimeout(()=>{refreshContent()},d.optional.expirationTimeSec*1000);
                     };
-                },()=>{
+                },(error)=>{
                     connectionFailedCounter ++;
                     if (connectionFailedCounter <= 3){ var timeoutSec = 30; }
                     else if (connectionFailedCounter <= 4){ var timeoutSec = 120; }
                     else if (connectionFailedCounter <= 6){ var timeoutSec = 20*60; }
                     else if (connectionFailedCounter <= 7){ var timeoutSec = 90*60; }
                     else { var timeoutSec = 240*60; }
-                    console.log("Loading " + name + " failed.");
-                    console.log("Refresh " + name + " in " + timeoutSec + " sec.")
+                    console.log("\n++++++++++++++++++++\nLoading " + name + " failed.");
+                    console.log(error);
+                    console.log("Refresh " + name + " in " + timeoutSec + " sec.\n++++++++++++++++++++")
                     setTimeout(()=>{refreshContent()},timeoutSec*1000);
                 })
                 .catch((exception)=>{console.log(exception);});
         };
 
-        //entry point
-        refreshContent();
     }
 }
