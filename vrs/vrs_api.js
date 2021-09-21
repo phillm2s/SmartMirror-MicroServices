13f3d5e89b3d7309a960443666135bf083281b8d
phillm2s <patrick.hillmann@smail.inf.h-brs.de>
2021-09-21 22:43:43 +0200

    Update vrs_api.js

--- vrs/vrs_api.js
+++ vrs/vrs_api.js
@@ -3,7 +3,7 @@
  * 
  * @param {string} url specific url from 'vrs.de'
  */
-function getTimetable(url="https://www.vrs.de/index?eID=tx_vrsinfo_ass2_departuremonitor&i=34a14adbc9c5eee8dd6ae160722676bc") {
+function getTimetable(url) {
     var proxy_url = "http://localhost:8081/cors?url="+url
 
     return fetch(proxy_url)
