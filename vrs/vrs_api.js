c91ea0ee6f284f203b112ece7cc4129d2ff5c336
phillm2s <patrick.hillmann@smail.inf.h-brs.de>
2021-09-21 22:40:24 +0200

    finished vrs service

--- vrs/vrs_api.js
+++ vrs/vrs_api.js
@@ -1,35 +1,12 @@
 
+/**
+ * 
+ * @param {string} url specific url from 'vrs.de'
+ */
+function getTimetable(url="https://www.vrs.de/index?eID=tx_vrsinfo_ass2_departuremonitor&i=34a14adbc9c5eee8dd6ae160722676bc") {
+    var proxy_url = "http://localhost:8081/cors?url="+url
 
-function getStationIDFromName(stationName) {
-    var url = "https://apitest.vrsinfo.de:4443/vrs/cgi/service/ass";
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-    
-    xhr.setRequestHeader("Content-Type", "application/xml");
-    
-    xhr.onreadystatechange = function () {
-       if (xhr.readyState === 4) {
-          console.log(xhr.status);
-          console.log(xhr.responseText);
-       }};
-    
-    var data = `<?xml version="1.0" encoding="ISO-8859-15"?>
-            <Request>
-            <ObjectInfo>
-            <ObjectSearch>
-            <String>bonn</String>
-            <Classes>
-            <Stop/>
-            </Classes>
-            </ObjectSearch>
-            <Options>
-            <Output>
-            <SRSName>urn:adv:crs:ETRS89_UTM32</SRSName>
-            </Output>
-            </Options>
-            </ObjectInfo>
-            </Request>`;
-    
-    xhr.send(data);
+    return fetch(proxy_url)
+    .then(res => res.json())
 }
+
