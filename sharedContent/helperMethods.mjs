d736b3754585aa47e5b2a5a7b902ce5ab9c1212c
Patrick <32835284+phillm2s@users.noreply.github.com>
2022-01-16 11:09:22 +0100

    change scaling behaviour for services

--- sharedContent/helperMethods.mjs
+++ sharedContent/helperMethods.mjs
@@ -4,17 +4,26 @@ export function sendIframeLoadedCallback() {
 
 
 /**
- * Scale the given element to 100% of its parent size
- * The caling is oriented only by the width ratio !
+ * Scale the given element to 100% of its parent size but in a way the content fits the parent.
+ * This means always scale by the smaler newHeight/oldHeight , newWeidth/oldWeidth ratio.
  * @param {HTMLElement} element
  * @param {HTMLElement} parent the parent element of 'element'
  */
 export function scaleElementToParentSize(element, parent) {
     //var heightScaling  = Math.round((parent.offsetHeight / element.offsetHeight)*100);
-    let widthScaling  = Math.round((parent.offsetWidth / element.offsetWidth)*100);
-    let heightScaling = widthScaling;
 
-    element.style.transform = "scale(" + widthScaling + "%," + heightScaling + "%)";
+    //scale element ONLY depending on the width
+    let scaleWidth  = Math.round((parent.offsetWidth / element.offsetWidth)*100);
+    let scaleHeight = Math.round((parent.offsetHeight / element.offsetHeight)*100);
+
+
+    //check if new scaled height would be overlap the parent to avoid cut of
+    let scaleRatio = scaleWidth;
+    if(scaleWidth > scaleHeight) {
+        scaleRatio = scaleHeight
+    }
+
+    element.style.transform = "scale(" + scaleRatio + "%," + scaleRatio + "%)";
     element.style.transformOrigin = "left top";
 }
 
