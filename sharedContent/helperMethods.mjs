16c2ea7fa7d2fa33b2a8cb98ef96da7104be0828
Patrick <32835284+phillm2s@users.noreply.github.com>
2022-01-16 20:33:47 +0100

    Add universal circuitBreaker helper method

--- sharedContent/helperMethods.mjs
+++ sharedContent/helperMethods.mjs
@@ -28,3 +28,37 @@ export function scaleElementToParentSize(element, parent) {
 }
 
 
+/**
+ * Resolve imidialty with the element returned by the given method if the given method resolves.
+ * If there is an error in the parameter method the system this method trys to invoke it again after the given timeout and increase the timeout index
+ * @param {function} func Any function.
+ * @param {number[]} timeouts The time on milliseconds waiting after a retry. If the number of retrys is bigger thant the amount of given timeouts the Promise rejects.
+ * @param {boolean} infinite Optional, if true the last element in timeouts will be used until success.
+ * @param {(retry: number, error: Error)=> void} failureCallback callback Function invoking after each failed attemp.
+ */
+ export async function circuitBreaker(func, timeouts, infinite=false, failureCallback=(retry, error)=>{}){
+    let timeoutCounter=0; // maxsize is number of elements in tomouts array
+    let realCounter=0; // increasing on every failure attemp
+    
+    while(timeoutCounter < timeouts.length){
+        try{
+            return await func();
+        } catch(err){
+            realCounter ++;
+
+            failureCallback(realCounter, err);
+            await new Promise(r=> setTimeout(r,timeouts[timeoutCounter]));
+
+            // infinite mode and last element?
+            if(infinite && timeoutCounter == timeouts.length-1){
+                // dont increase;
+            } else {
+                timeoutCounter++;
+            }
+
+                
+        }
+    }
+}
+
+
